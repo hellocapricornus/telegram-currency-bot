2b3ccc517215c6a839f8a685dafa8aaadd3aefc1
hellocapricornus <hellocapricornus@gmail.com>
2025-08-23 23:42:12 +0800

    Update anti_ads.py

--- handlers/anti_ads.py
+++ handlers/anti_ads.py
@@ -8,7 +8,7 @@ from telegram.constants import ChatMemberStatus
 # 广告关键词正则
 AD_PATTERNS = [
     r"discord\.gg", r"https?://",
-    r"加群", r"推广", r"福利", r"邀请码", r"交流群", r"官方", r"投资", r"刷单", r"返利",
+    r"加群", r"推广", r"福利", r"邀请码", r"交流群", r"官方", r"投资", r"返利",
     r"代开", r"代办", r"代用户名", r"tg群", r"VX", r"vx", r"微信",
 ]
 AD_REGEX = re.compile("|".join(AD_PATTERNS), flags=re.IGNORECASE)
